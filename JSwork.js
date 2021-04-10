675b4334c6d9259986d63ff8fe7c364dc4632fc2
Mukolas <dinozavrickraptor@gmail.com>
2021-04-10 19:28:11 +0300

    11

--- JSwork.js
+++ JSwork.js
@@ -112,11 +112,11 @@ towar={
         cost: 500
     }
 ]
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+fetch('https://github.com/Mukolas/Diplomna/blob/main/Tow.json')
   .then(response => response.json())
   .then(json => console.log(json.id))
 
-  
+
 /*Функція створення товару */
     
     for(i=0;i<skladPPT.length;i++){
